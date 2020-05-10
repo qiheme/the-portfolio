bd22499118be58706b1d7ceff4b2c683b86ca62f
Quincy Iheme <qiheme@gmail.com>
2020-05-09 23:32:02 -0400

    Convert about page to react

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
-import Home from './pages/Home/Home';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import About from './pages/About/About';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Home />
+    <>
+    <Router>
+        <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route>
+          {"Not found"}
+        </Route>
+      </Switch>
     </div>
+  </Router>
+  </>
   );
 }
 
---
+++ src/pages/About/About.tsx
@@ -0,0 +1,514 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <div className="fh5co-loader"></div>
+
+      <div id="page">
+        <nav className="fh5co-nav" role="navigation">
+          <div className="top-menu">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-2">
+                  <div id="fh5co-logo">
+                    <a href="index.html">
+                      Air<span>.</span>
+                    </a>
+                  </div>
+                </div>
+                <div className="col-xs-10 text-right menu-1">
+                  <ul>
+                    <li>
+                      <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                      <a href="portfolio.html">Portfolio</a>
+                    </li>
+                    <li className="has-dropdown">
+                      <a href="blog.html">Blog</a>
+                      <ul className="dropdown">
+                        <li>
+                          <a href="#">Web Design</a>
+                        </li>
+                        <li>
+                          <a href="#">eCommerce</a>
+                        </li>
+                        <li>
+                          <a href="#">Branding</a>
+                        </li>
+                        <li>
+                          <a href="#">API</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li className="active">
+                      <a href="about.html">About</a>
+                    </li>
+                    <li>
+                      <a href="contact.html">Contact</a>
+                    </li>
+                    <li className="btn-cta">
+                      <a href="#">
+                        <span>Login</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <div id="fh5co-about">
+          <div className="container">
+            <div className="row animate-box">
+              <div className="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                <h2>About Us</h2>
+                <p>
+                  Far far away, behind the word mountains, far from the
+                  countries Vokalia and Consonantia.
+                </p>
+              </div>
+            </div>
+            <div className="row animate-box">
+              <div className="col-md-6 col-md-offset-3 text-center heading-section">
+                <h3>Our History</h3>
+                <p>
+                  Far far away, behind the word mountains, far from the
+                  countries Vokalia and Consonantia, there live the blind texts.
+                </p>
+              </div>
+            </div>
+
+            <div className="row row-bottom-padded-md">
+              <div className="col-md-12 text-center animate-box">
+                <p>
+                  <img
+                    src="images/cover_bg_1.jpg"
+                    alt="Free HTML5 Bootstrap Template"
+                    className="img-responsive img-rounded"
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="fh5co-features">
+          <div className="container">
+            <div className="services-padding">
+              <div className="row">
+                <div className="col-md-4 animate-box">
+                  <div className="feature-left">
+                    <span className="icon">
+                      <i className="icon-hotairballoon"></i>
+                    </span>
+                    <div className="feature-copy">
+                      <h3>Marketing</h3>
+                      <p>
+                        Facilis ipsum reprehenderit nemo molestias. Aut cum
+                        mollitia reprehenderit.
+                      </p>
+                      <p>
+                        <a href="#">
+                          Learn More <i className="icon-arrow-right22"></i>
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-4 animate-box">
+                  <div className="feature-left">
+                    <span className="icon">
+                      <i className="icon-search"></i>
+                    </span>
+                    <div className="feature-copy">
+                      <h3>Search Engine</h3>
+                      <p>
+                        Facilis ipsum reprehenderit nemo molestias. Aut cum
+                        mollitia reprehenderit.
+                      </p>
+                      <p>
+                        <a href="#">
+                          Learn More <i className="icon-arrow-right22"></i>
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-4 animate-box">
+                  <div className="feature-left">
+                    <span className="icon">
+                      <i className="icon-wallet"></i>
+                    </span>
+                    <div className="feature-copy">
+                      <h3>Earn Money</h3>
+                      <p>
+                        Facilis ipsum reprehenderit nemo molestias. Aut cum
+                        mollitia reprehenderit.
+                      </p>
+                      <p>
+                        <a href="#">
+                          Learn More <i className="icon-arrow-right22"></i>
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-4 animate-box">
+                  <div className="feature-left">
+                    <span className="icon">
+                      <i className="icon-wine"></i>
+                    </span>
+                    <div className="feature-copy">
+                      <h3>Entrepreneur</h3>
+                      <p>
+                        Facilis ipsum reprehenderit nemo molestias. Aut cum
+                        mollitia reprehenderit.
+                      </p>
+                      <p>
+                        <a href="#">
+                          Learn More <i className="icon-arrow-right22"></i>
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-4 animate-box">
+                  <div className="feature-left">
+                    <span className="icon">
+                      <i className="icon-genius"></i>
+                    </span>
+                    <div className="feature-copy">
+                      <h3>Stragic Plan</h3>
+                      <p>
+                        Facilis ipsum reprehenderit nemo molestias. Aut cum
+                        mollitia reprehenderit.
+                      </p>
+                      <p>
+                        <a href="#">
+                          Learn More <i className="icon-arrow-right22"></i>
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-4 animate-box">
+                  <div className="feature-left">
+                    <span className="icon">
+                      <i className="icon-chat"></i>
+                    </span>
+                    <div className="feature-copy">
+                      <h3>Support</h3>
+                      <p>
+                        Facilis ipsum reprehenderit nemo molestias. Aut cum
+                        mollitia reprehenderit.
+                      </p>
+                      <p>
+                        <a href="#">
+                          Learn More <i className="icon-arrow-right22"></i>
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="fh5co-content-section">
+          <div className="container">
+            <div className="row row-bottom-padded-md">
+              <div className="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+                <h3>Our Team</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Velit est facilis maiores, perspiciatis accusamus asperiores
+                  sint consequuntur debitis.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4">
+                <div className="fh5co-team text-center animate-box">
+                  <figure>
+                    <img src="images/user-1.jpg" alt="user" />
+                  </figure>
+                  <p>
+                    Jean Doe, XYZ Co. <br />{" "}
+                    <span className="text-mute">Designer</span>
+                  </p>
+                  <blockquote>
+                    <p>
+                      Far far away, behind the word mountains, far from the
+                      countries Vokalia and Consonantia, there live the blind
+                      texts. Far far away, behind the word mountains, far from
+                      the countries Vokalia and Consonantia, there live the
+                      blind texts.
+                    </p>
+                  </blockquote>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="fh5co-team text-center animate-box">
+                  <figure>
+                    <img src="images/user-2.jpg" alt="user" />
+                  </figure>
+                  <p>
+                    Jean Doe, XYZ Co. <br />{" "}
+                    <span className="text-mute">Developer</span>
+                  </p>
+                  <blockquote>
+                    <p>
+                      Far far away, behind the word mountains, far from the
+                      countries Vokalia and Consonantia, there live the blind
+                      texts. Far far away, behind the word mountains, far from
+                      the countries Vokalia and Consonantia, there live the
+                      blind texts.
+                    </p>
+                  </blockquote>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="fh5co-team text-center animate-box">
+                  <figure>
+                    <img src="images/user-3.jpg" alt="user" />
+                  </figure>
+                  <p>
+                    Jean Doe, XYZ Co. <br />{" "}
+                    <span className="text-mute">Photographer</span>
+                  </p>
+                  <blockquote>
+                    <p>
+                      Far far away, behind the word mountains, far from the
+                      countries Vokalia and Consonantia, there live the blind
+                      texts. Far far away, behind the word mountains, far from
+                      the countries Vokalia and Consonantia, there live the
+                      blind texts.
+                    </p>
+                  </blockquote>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-4">
+                <div className="fh5co-team text-center animate-box">
+                  <figure>
+                    <img src="images/user-1.jpg" alt="user" />
+                  </figure>
+                  <p>
+                    Jean Doe, XYZ Co. <br />{" "}
+                    <span className="text-mute">Designer</span>
+                  </p>
+                  <blockquote>
+                    <p>
+                      Far far away, behind the word mountains, far from the
+                      countries Vokalia and Consonantia, there live the blind
+                      texts. Far far away, behind the word mountains, far from
+                      the countries Vokalia and Consonantia, there live the
+                      blind texts.
+                    </p>
+                  </blockquote>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="fh5co-team text-center animate-box">
+                  <figure>
+                    <img src="images/user-2.jpg" alt="user" />
+                  </figure>
+                  <p>
+                    Jean Doe, XYZ Co. <br />{" "}
+                    <span className="text-mute">Developer</span>
+                  </p>
+                  <blockquote>
+                    <p>
+                      Far far away, behind the word mountains, far from the
+                      countries Vokalia and Consonantia, there live the blind
+                      texts. Far far away, behind the word mountains, far from
+                      the countries Vokalia and Consonantia, there live the
+                      blind texts.
+                    </p>
+                  </blockquote>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="fh5co-team text-center animate-box">
+                  <figure>
+                    <img src="images/user-3.jpg" alt="user" />
+                  </figure>
+                  <p>
+                    Jean Doe, XYZ Co. <br />{" "}
+                    <span className="text-mute">Photographer</span>
+                  </p>
+                  <blockquote>
+                    <p>
+                      Far far away, behind the word mountains, far from the
+                      countries Vokalia and Consonantia, there live the blind
+                      texts. Far far away, behind the word mountains, far from
+                      the countries Vokalia and Consonantia, there live the
+                      blind texts.
+                    </p>
+                  </blockquote>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="fh5co-started">
+          <div className="overlay"></div>
+          <div className="container">
+            <div className="row animate-box">
+              <div className="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                <h2>Hire Us!</h2>
+                <p>
+                  Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia
+                  reprehenderit. Eos cumque dicta adipisci architecto culpa
+                  amet.
+                </p>
+                <p>
+                  <a href="#" className="btn btn-default btn-lg">
+                    Contact Us
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <footer id="fh5co-footer" role="contentinfo">
+          <div className="container">
+            <div className="row row-pb-md">
+              <div className="col-md-4 fh5co-widget">
+                <h4>Air</h4>
+                <p>
+                  Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia
+                  reprehenderit. Eos cumque dicta adipisci architecto culpa
+                  amet.
+                </p>
+              </div>
+              <div className="col-md-2 col-md-push-1 fh5co-widget">
+                <h4>Links</h4>
+                <ul className="fh5co-footer-links">
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">Portfolio</a>
+                  </li>
+                  <li>
+                    <a href="#">Blog</a>
+                  </li>
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="col-md-2 col-md-push-1 fh5co-widget">
+                <h4>Categories</h4>
+                <ul className="fh5co-footer-links">
+                  <li>
+                    <a href="#">Landing Page</a>
+                  </li>
+                  <li>
+                    <a href="#">Real Estate</a>
+                  </li>
+                  <li>
+                    <a href="#">Personal</a>
+                  </li>
+                  <li>
+                    <a href="#">Business</a>
+                  </li>
+                  <li>
+                    <a href="#">e-Commerce</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="col-md-4 col-md-push-1 fh5co-widget">
+                <h4>Contact Information</h4>
+                <ul className="fh5co-footer-links">
+                  <li>
+                    198 West 21th Street, <br /> Suite 721 New York NY 10016
+                  </li>
+                  <li>
+                    <a href="tel://1234567920">+ 1235 2355 98</a>
+                  </li>
+                  <li>
+                    <a href="mailto:info@yoursite.com">info@yoursite.com</a>
+                  </li>
+                  <li>
+                    <a href="http://https://freehtml5.co">freehtml5.co</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="row copyright">
+              <div className="col-md-12 text-center">
+                <p>
+                  <small className="block">
+                    &copy; 2016 Free HTML5. All Rights Reserved.
+                  </small>
+                  <small className="block">
+                    Designed by{" "}
+                    <a href="http://freehtml5.co/" target="_blank">
+                      FreeHTML5.co
+                    </a>{" "}
+                    Demo Images:{" "}
+                    <a href="http://unsplash.co/" target="_blank">
+                      Unsplash
+                    </a>
+                  </small>
+                </p>
+                <p>
+                  <ul className="fh5co-social-icons">
+                    <li>
+                      <a href="#">
+                        <i className="icon-twitter"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i className="icon-facebook"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i className="icon-linkedin"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i className="icon-dribbble"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+
+      <div className="gototop js-top">
+        <a href="#" className="js-gotop">
+          <i className="icon-arrow-up22"></i>
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default About;
