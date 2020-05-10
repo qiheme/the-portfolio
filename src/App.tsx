17cac3f862bf006a5e4f3ee1395a6834acf71452
Quincy Iheme <qiheme@gmail.com>
2020-05-10 08:37:36 -0400

    Add contact and portfolio pages to react

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,38 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
-import About from './pages/About/About';
-import Home from './pages/Home/Home';
-import Blog from './pages/Blog/Blog';
+import "./App.css";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+import Blog from "./pages/Blog/Blog";
+import Contact from "./pages/Contact/Contact";
+import Portfolio from "./pages/Portfolio/Portfolio";
 
 function App() {
   return (
     <>
-    <Router>
+      <Router>
         <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/blog">
-          <Blog />
-        </Route>
-        <Route>
-          {"Not found"}
-        </Route>
-      </Switch>
-    </div>
-  </Router>
-  </>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/blog">
+              <Blog />
+            </Route>
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+            <Route exact path="/portfolio">
+              <Portfolio />
+            </Route>
+            <Route>{"Not found"}</Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Portfolio/Portfolio.tsx
@@ -0,0 +1,369 @@
+import React from "react";
+
+function Portfolio() {
+  return (
+    <>
+      <div className="fh5co-loader"></div>
+
+      <div id="page">
+        <nav className="fh5co-nav" role="navigation">
+          <div className="top-menu">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-2">
+                  <div id="fh5co-logo">
+                    <a href="index.html">
+                      Air<span>.</span>
+                    </a>
+                  </div>
+                </div>
+                <div className="col-xs-10 text-right menu-1">
+                  <ul>
+                    <li>
+                      <a href="index.html">Home</a>
+                    </li>
+                    <li className="active">
+                      <a href="portfolio.html">Portfolio</a>
+                    </li>
+                    <li className="has-dropdown">
+                      <a href="blog.html">Blog</a>
+                      <ul className="dropdown">
+                        <li>
+                          <a href="#">Web Design</a>
+                        </li>
+                        <li>
+                          <a href="#">eCommerce</a>
+                        </li>
+                        <li>
+                          <a href="#">Branding</a>
+                        </li>
+                        <li>
+                          <a href="#">API</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <a href="about.html">About</a>
+                    </li>
+                    <li>
+                      <a href="contact.html">Contact</a>
+                    </li>
+                    <li className="btn-cta">
+                      <a href="#">
+                        <span>Login</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <div id="fh5co-portfolio">
+          <div className="container">
+            <div className="row animate-box">
+              <div className="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                <h2>Portfolio</h2>
+                <p>
+                  Far far away, behind the word mountains, far from the
+                  countries Vokalia and Consonantia.
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-4">
+                <div className="fh5co-portfolio animate-box">
+                  <a href="#">
+                    <div
+                      className="portfolio-entry"
+                      style={{ backgroundImage: "url(images/portfolio-1.jpg)" }}
+                    ></div>
+                    <div className="portfolio-text">
+                      <h3>Photoshoot On The Street</h3>
+                      <ul className="stuff">
+                        <li>
+                          <i className="icon-heart2"></i>200
+                        </li>
+                        <li>
+                          <i className="icon-eye2"></i>248
+                        </li>
+                        <li>
+                          <i className="icon-download3"></i>240
+                        </li>
+                      </ul>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="fh5co-portfolio animate-box">
+                  <a href="#">
+                    <div
+                      className="portfolio-entry"
+                      style={{ backgroundImage: "url(images/portfolio-2.jpg)" }}
+                    ></div>
+                    <div className="portfolio-text">
+                      <h3>Photoshoot On The Street</h3>
+                      <ul className="stuff">
+                        <li>
+                          <i className="icon-heart2"></i>200
+                        </li>
+                        <li>
+                          <i className="icon-eye2"></i>248
+                        </li>
+                        <li>
+                          <i className="icon-download3"></i>240
+                        </li>
+                      </ul>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="fh5co-portfolio animate-box">
+                  <a href="#">
+                    <div
+                      className="portfolio-entry"
+                      style={{ backgroundImage: "url(images/portfolio-3.jpg)" }}
+                    ></div>
+                    <div className="portfolio-text">
+                      <h3>Photoshoot On The Street</h3>
+                      <ul className="stuff">
+                        <li>
+                          <i className="icon-heart2"></i>200
+                        </li>
+                        <li>
+                          <i className="icon-eye2"></i>248
+                        </li>
+                        <li>
+                          <i className="icon-download3"></i>240
+                        </li>
+                      </ul>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="fh5co-portfolio animate-box">
+                  <a href="#">
+                    <div
+                      className="portfolio-entry"
+                      style={{ backgroundImage: "url(images/portfolio-5.jpg)" }}
+                    ></div>
+                    <div className="portfolio-text">
+                      <h3>Photoshoot On The Street</h3>
+                      <ul className="stuff">
+                        <li>
+                          <i className="icon-heart2"></i>200
+                        </li>
+                        <li>
+                          <i className="icon-eye2"></i>248
+                        </li>
+                        <li>
+                          <i className="icon-download3"></i>240
+                        </li>
+                      </ul>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="fh5co-portfolio animate-box">
+                  <a href="#">
+                    <div
+                      className="portfolio-entry"
+                      style={{ backgroundImage: "url(images/portfolio-4.jpg)" }}
+                    ></div>
+                    <div className="portfolio-text">
+                      <h3>Photoshoot On The Street</h3>
+                      <ul className="stuff">
+                        <li>
+                          <i className="icon-heart2"></i>200
+                        </li>
+                        <li>
+                          <i className="icon-eye2"></i>248
+                        </li>
+                        <li>
+                          <i className="icon-download3"></i>240
+                        </li>
+                      </ul>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="fh5co-portfolio animate-box">
+                  <a href="#">
+                    <div
+                      className="portfolio-entry"
+                      style={{ backgroundImage: "url(images/portfolio-6.jpg)" }}
+                    ></div>
+                    <div className="portfolio-text">
+                      <h3>Photoshoot On The Street</h3>
+                      <ul className="stuff">
+                        <li>
+                          <i className="icon-heart2"></i>200
+                        </li>
+                        <li>
+                          <i className="icon-eye2"></i>248
+                        </li>
+                        <li>
+                          <i className="icon-download3"></i>240
+                        </li>
+                      </ul>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="fh5co-started">
+          <div className="overlay"></div>
+          <div className="container">
+            <div className="row animate-box">
+              <div className="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                <h2>Hire Us!</h2>
+                <p>
+                  Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia
+                  reprehenderit. Eos cumque dicta adipisci architecto culpa
+                  amet.
+                </p>
+                <p>
+                  <a href="#" className="btn btn-default btn-lg">
+                    Contact Us
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <footer id="fh5co-footer" role="contentinfo">
+          <div className="container">
+            <div className="row row-pb-md">
+              <div className="col-md-4 fh5co-widget">
+                <h4>Air</h4>
+                <p>
+                  Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia
+                  reprehenderit. Eos cumque dicta adipisci architecto culpa
+                  amet.
+                </p>
+              </div>
+              <div className="col-md-2 col-md-push-1 fh5co-widget">
+                <h4>Links</h4>
+                <ul className="fh5co-footer-links">
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">Portfolio</a>
+                  </li>
+                  <li>
+                    <a href="#">Blog</a>
+                  </li>
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="col-md-2 col-md-push-1 fh5co-widget">
+                <h4>Categories</h4>
+                <ul className="fh5co-footer-links">
+                  <li>
+                    <a href="#">Landing Page</a>
+                  </li>
+                  <li>
+                    <a href="#">Real Estate</a>
+                  </li>
+                  <li>
+                    <a href="#">Personal</a>
+                  </li>
+                  <li>
+                    <a href="#">Business</a>
+                  </li>
+                  <li>
+                    <a href="#">e-Commerce</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="col-md-4 col-md-push-1 fh5co-widget">
+                <h4>Contact Information</h4>
+                <ul className="fh5co-footer-links">
+                  <li>
+                    198 West 21th Street, <br /> Suite 721 New York NY 10016
+                  </li>
+                  <li>
+                    <a href="tel://1234567920">+ 1235 2355 98</a>
+                  </li>
+                  <li>
+                    <a href="mailto:info@yoursite.com">info@yoursite.com</a>
+                  </li>
+                  <li>
+                    <a href="http://https://freehtml5.co">freehtml5.co</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="row copyright">
+              <div className="col-md-12 text-center">
+                <p>
+                  <small className="block">
+                    &copy; 2016 Free HTML5. All Rights Reserved.
+                  </small>
+                  <small className="block">
+                    Designed by{" "}
+                    <a href="http://freehtml5.co/" target="_blank">
+                      FreeHTML5.co
+                    </a>{" "}
+                    Demo Images:{" "}
+                    <a href="http://unsplash.co/" target="_blank">
+                      Unsplash
+                    </a>
+                  </small>
+                </p>
+                <p>
+                  <ul className="fh5co-social-icons">
+                    <li>
+                      <a href="#">
+                        <i className="icon-twitter"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i className="icon-facebook"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i className="icon-linkedin"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i className="icon-dribbble"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+
+      <div className="gototop js-top">
+        <a href="#" className="js-gotop">
+          <i className="icon-arrow-up22"></i>
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Portfolio;
